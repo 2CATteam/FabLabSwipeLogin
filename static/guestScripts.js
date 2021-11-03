a70f988971fd96d06ae935cb93a8f36881da8ad7
Daniel Royer <2CATteam@gmail.com>
2021-11-03 14:49:31 -0500

    Fixed guest registration, fixed non-OU crashing

--- static/guestScripts.js
+++ static/guestScripts.js
@@ -99,12 +99,14 @@ $(document).ready(() => {
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault()
-            event.stopPropagation()
-    
-            form.classList.add('was-validated')
-        }, false)
+            form.addEventListener('submit', function (event) {
+                event.preventDefault()
+                if (!form.checkValidity()) {
+                    event.stopPropagation()
+                }
+        
+                form.classList.add('was-validated')
+            }, false)
         })
     window.scrollTo({left: window.innerWidth * 2, top: 0, behavior: "instant"})
 })
